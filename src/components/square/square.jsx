2a88ef4623b9fafde95014dd5acf20287d29d62d
AlexJ <43654720+hyperxtend@users.noreply.github.com>
2020-05-13 13:09:52 +0200

    fix: Added Eslint and fixed linting errors

--- src/components/square/square.jsx
+++ src/components/square/square.jsx
@@ -2,16 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
 
- const Square = (props) => {
-    return(  
-    <Button variant="secondary outline-dark" className="square" onClick={props.onClick}>
-      {props.value}
-    </Button>)
-    }
+ const Square = (props) => (
+   <Button
+     variant="secondary outline-dark"
+     className="square"
+     onClick={props.onClick}
+   >
+     {props.value}
+   </Button>
+);
 
     Square.propTypes = {
       onClick: PropTypes.func,
       value: PropTypes.string
+    };
+
+    Square.defaultProps = {
+      onClick: () => {},
+      value: 'X'
     };
 
 export default Square;
--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './index.css';
-import serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 import Game from './components/game';
 
 ReactDOM.render(<Game />, document.getElementById('root'));
