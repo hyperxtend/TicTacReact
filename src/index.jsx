e74a6993512021037f06659a15a3d393d0da88a9
AlexJ <43654720+hyperxtend@users.noreply.github.com>
2020-05-14 10:00:18 +0200

    fix: Resolved merge conflicts

--- src/index.jsx
+++ src/index.jsx
@@ -6,42 +6,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './index.css';
 import ticTacToeApp from './reducers';
-<<<<<<< HEAD
-<<<<<<< HEAD:src/index.jsx
-import { GamePlay } from './components/game/container';
-
-
-const store = createStore(ticTacToeApp);
-=======
-import * as serviceWorker from './serviceWorker';
-
-=======
-import * as serviceWorker from './serviceWorker';
->>>>>>> e3b0c9b03b5930a9799f3ef2b3d483eadb8941eb
 import GamePlay from './components/game/container';
+import * as serviceWorker from './serviceWorker';
 
 const store = createStore(ticTacToeApp);
 const root = document.getElementById('root');
-<<<<<<< HEAD
->>>>>>> fix: Fixed default export and import statements:src/index.js
-=======
->>>>>>> e3b0c9b03b5930a9799f3ef2b3d483eadb8941eb
 
 render(
   <Provider store={store}>
     <GamePlay />
   </Provider>,
-<<<<<<< HEAD
-<<<<<<< HEAD:src/index.jsx
-  document.getElementById('root')
-)
-=======
   root
 );
- serviceWorker.unregister();
->>>>>>> fix: Fixed default export and import statements:src/index.js
-=======
-  root
-);
- serviceWorker.unregister();
->>>>>>> e3b0c9b03b5930a9799f3ef2b3d483eadb8941eb
+
+serviceWorker.unregister();
