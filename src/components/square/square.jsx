11ac1f3e384173f6a95b9c7e0609848f950c0be4
AlexJ <43654720+hyperxtend@users.noreply.github.com>
2020-05-19 15:50:23 +0200

    fix: Destructured props for component

--- src/components/square/square.jsx
+++ src/components/square/square.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
 
- const Square = (props) => (
+ const Square = ({ onClick, value }) => (
    <Button
      variant="secondary outline-dark"
      className="square"
-     onClick={props.onClick}
+     onClick={onClick}
    >
-     {props.value}
+     {value}
    </Button>
 );
 
