29ddfe65b793898349f1c4eb594ad61fc5e0379f
AlexJ <43654720+hyperxtend@users.noreply.github.com>
2020-05-20 12:45:19 +0200

    fix: Updated module and component for app to work

--- src/components/game/component.jsx
+++ src/components/game/component.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
+import { Button, Container } from 'react-bootstrap';
 import PropTypes from 'prop-types';
-import { Container, Button } from 'react-bootstrap';
 
 import Board from '../board';
 import calculateWinner from '../../utilities';
 
-
-const Game = ({ history,
-  jumpTo,
-  movesAscendingOrder,
-  onClick,
-  sortMoves,
+const Game = ({
+  history,
   stepNumber,
-  xIsNext },
-  props) => {
-    const { moveHistory } = props.history;
-    const current = moveHistory[stepNumber];
+  jumpTo,
+  sortMoves,
+  xIsNext,
+  movesAscendingOrder,
+  onClick
+}) => {
+    const movesHistory = history;
+    const current = movesHistory[stepNumber];
     const winner = calculateWinner(current.squares);
-    const moves = history.map((step, move) => {
+    const moves = movesHistory.map((step, move) => {
     const desc = move ? `Go to move #${move}` : 'Restart';
+
       return (
-        <span key={[move]} className={(stepNumber === move ? 'current' : '')}>
+        <span key={[move]}>
           <Button
             size="sm"
             variant="outline-dark"
@@ -32,18 +33,16 @@ const Game = ({ history,
       );
     });
 
-    let status = calculateWinnerStatus(winner);
-
-    function calculateWinnerStatus() {
-        if (winner) {
-          status = `${winner.winnerName} is the Winner!`;
-        } else {
-          status = `Next player is  ${xIsNext ? 'X' : 'O'}`;
-        }
-        if (!winner && stepNumber === 9) {
-          status = 'It\'s a Draw!';
-        }
+    let status;
+    if (winner) {
+      status = `${winner.winnerName} is the Winner!`;
+    } else {
+      status = `Next player is  ${xIsNext ? 'X' : 'O'}`;
     }
+    if (!winner && stepNumber === 9) {
+      status = 'It\'s a Draw!';
+    }
+
     return (
       <Container className="game">
         <div className="gameBoard">
@@ -66,7 +65,7 @@ const Game = ({ history,
 
   Game.propTypes = {
     jumpTo: PropTypes.func,
-    movesAscendingOrder: PropTypes.func,
+    movesAscendingOrder: PropTypes.bool,
     onClick: PropTypes.func,
     sortMoves: PropTypes.func,
     stepNumber: PropTypes.number,
@@ -75,7 +74,7 @@ const Game = ({ history,
 
 Game.defaultProps = {
   jumpTo: () => {},
-  movesAscendingOrder: () => {},
+  movesAscendingOrder: true,
   onClick: () => {},
   sortMoves: () => {},
   stepNumber: 0,
