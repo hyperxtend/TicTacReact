fc453aad04ed2bb957a4f1f00b23b5043113e994
Alex Joshua <ialexjoshua@gmail.com>
2020-05-21 16:58:45 +0200

    fix: Removed test to check if Board exists

--- src/components/board/board.test.jsx
+++ src/components/board/board.test.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 
 import Board from './board';
 
 describe('<Board/>', () => {
-  it('checks if component exists', () => {
-    const squares = Array(9).fill('X');
-    const component = shallow(<Board squares={squares} />);
-    expect(component.exists()).toBe(true);
-  });
 
   it('onClick event works on buttons', () => {
     const onClick = jest.fn();
-     const squares = Array(9).fill('O');
+     const squares = Array(9).fill('');
      const wrapper = mount(<Board squares={squares} onClick={onClick} />);
     wrapper
       .find('button.square')
