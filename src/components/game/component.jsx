5c636be574d8c12e193773be0f46a5c00d8df18a
Alex Joshua <ialexjoshua@gmail.com>
2020-05-26 16:45:52 +0200

    fix: Split proptypes for history and squares

--- src/components/game/component.jsx
+++ src/components/game/component.jsx
@@ -8,21 +8,19 @@ const Game = ({ history, stepNumber }) => {
   const current = history[stepNumber];
   return (
     <Container className="game">
-      <Board squares={current} />
+      <Board squares={current.squares} />
     </Container>
   );
 };
 
 Game.propTypes = {
-  history: PropTypes.arrayOf(
-    PropTypes.shape({
-      squares: PropTypes.arrayOf(PropTypes.string).isRequired,
-    })
-  ),
+  history: PropTypes.arrayOf(PropTypes.object),
+  squares: PropTypes.arrayOf(PropTypes.string),
   stepNumber: PropTypes.number,
 };
 Game.defaultProps = {
-  history: [],
+  history: [{}],
+  squares: [''],
   stepNumber: 0,
 };
 
