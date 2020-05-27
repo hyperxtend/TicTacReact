064b197a71a405035d41d99fcd0f3eb148700f77
Alex Joshua <ialexjoshua@gmail.com>
2020-05-27 13:02:39 +0200

    fix: Added map & function for move history

--- src/components/game/component.jsx
+++ src/components/game/component.jsx
@@ -1,67 +1,32 @@
 import React from 'react';
-import { Button, Container } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 import Board from '../board';
-import calculateWinner from '../../utilities';
 
-const Game = ({
-  history,
-  stepNumber,
-  jumpTo,
-  sortMoves,
-  xIsNext,
-  movesAscendingOrder,
-  onClick,
-}) => {
-  const movesHistory = history;
-  const current = movesHistory[stepNumber];
-  const winner = calculateWinner(current.squares);
-  const moves = movesHistory.map((step, move) => {
-    const desc = move ? `Go to move #${move}` : 'Restart';
-
+const Game = ({ history, moveNumber, onClick, jumpTo }) => {
+  const current = history[moveNumber];
+  const moveMap = history.map((eachMove, moveNumber) => {
+    const moveHistory = moveNumber ? `Go to move #${moveNumber}` : 'Restart';
     return (
-      <span key={[move]}>
-        <Button size="sm" variant="outline-dark" onClick={() => jumpTo(move)}>
-          {desc}
-        </Button>
-      </span>
+      <Button
+        size="sm"
+        variant="outline-dark"
+        key={[moveNumber]}
+        onClick={() => jumpTo(moveNumber)}
+      >
+        {moveHistory}
+      </Button>
     );
   });
 
-  const calculateWinnerStatus = () => {
-    if (winner) {
-      return <p>{winner.winnerName} is the Winner!</p>;
-    }
-    if (!winner && stepNumber === 9) {
-      return <p>Its a Draw!</p>;
-    }
-    return <p>Next player is {xIsNext ? 'X' : 'O'}</p>;
-  };
-  const gameStatus = calculateWinnerStatus(winner);
   return (
     <Container className="game">
-      <div className="gameBoard">
-        <Board
-          squares={current.squares}
-          onClick={(squareIndex) => onClick(squareIndex)}
-          winnerCombination={winner ? winner.winnerCombination : []}
-        />
-      </div>
-      <div className="gameInfo">
-        <div className="playerStatus">{gameStatus}</div>
-      </div>
-      <span className="playerMoves">
-        {movesAscendingOrder ? moves : moves.reverse()}
-      </span>
-      <Button
-        size="sm"
-        variant="dark"
-        onClick={() => sortMoves()}
-        className="sortButton"
-      >
-        Change order
-      </Button>
+      <div className="playerMoves">{moveMap}</div>
+      <Board
+        squares={current.squares}
+        onClick={(currentSquare) => onClick(currentSquare)}
+      />
     </Container>
   );
 };
@@ -72,26 +37,19 @@ Game.propTypes = {
       squares: PropTypes.arrayOf(PropTypes.string),
     })
   ),
-  jumpTo: PropTypes.func,
-  movesAscendingOrder: PropTypes.bool,
+  moveNumber: PropTypes.number,
   onClick: PropTypes.func,
-  sortMoves: PropTypes.func,
-  stepNumber: PropTypes.number,
-  xIsNext: PropTypes.bool,
+  jumpTo: PropTypes.func,
 };
-
 Game.defaultProps = {
   history: [
     {
-      squares: [''],
+      squares: [],
     },
   ],
-  jumpTo: () => {},
-  movesAscendingOrder: true,
+  moveNumber: 0,
   onClick: () => {},
-  sortMoves: () => {},
-  stepNumber: 0,
-  xIsNext: true,
+  jumpTo: () => {},
 };
 
 export default Game;
