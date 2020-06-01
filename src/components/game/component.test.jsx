1d7285a37e2819153363d1506cc1ce8c8b770811
Alex <ialexjoshua@gmail.com>
2020-06-01 15:10:26 +0200

    fix: Removed unnecessary code in tests

--- src/components/game/component.test.jsx
+++ src/components/game/component.test.jsx
@@ -1,77 +1,62 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 
 import Board from '../board';
 
 import Game from './component';
 
-describe('<Game />', () => {
-  let game;
-  let board;
-  beforeAll(() => {
-    game = shallow(<Game />);
-    board = shallow(<Board />);
-  });
-  it('checks if game component exists', () => {
-    expect(game.exists()).toBe(true);
-  });
-  it('checks if board component exists', () => {
-    expect(board.exists()).toBe(true);
-  });
-});
-
 describe('<Game/>', () => {
   let wrapper;
   const mockOnClick = jest.fn();
-  const MOVE_VALUES_ONE = ['X', '', '', '', '', '', '', '', ''];
-  const MOVE_ONE = { squares: MOVE_VALUES_ONE };
+  const MoveValuesOne = ['X', '', '', '', '', '', '', '', ''];
+  const MoveONE = { squares: MoveValuesOne };
 
-  const MOVE_VALUES_TWO = ['X', 'O', '', '', '', '', '', '', ''];
-  const MOVE_TWO = { squares: MOVE_VALUES_TWO };
+  const MoveValuesTwo = ['X', 'O', '', '', '', '', '', '', ''];
+  const MoveTwo = { squares: MoveValuesTwo };
 
-  const MOVE_VALUES_THREE = ['X', 'O', 'X', '', '', '', '', '', ''];
-  const MOVE_THREE = { squares: MOVE_VALUES_THREE };
+  const MoveValuesThree = ['X', 'O', 'X', '', '', '', '', '', ''];
+  const MoveThree = { squares: MoveValuesThree };
 
-  const MOVE_VALUES_FOUR = ['X', 'O', 'X', '', 'O', '', '', '', ''];
-  const MOVE_FOUR = { squares: MOVE_VALUES_FOUR };
+  const MoveValuesFour = ['X', 'O', 'X', '', 'O', '', '', '', ''];
+  const MoveFour = { squares: MoveValuesFour };
 
-  const MOVE_VALUES_FIVE = ['X', 'O', 'X', '', 'O', '', '', 'X', ''];
-  const MOVE_FIVE = { squares: MOVE_VALUES_FIVE };
+  const MoveValuesFive = ['X', 'O', 'X', '', 'O', '', '', 'X', ''];
+  const MoveFive = { squares: MoveValuesFive };
 
-  const MOVE_VALUES_SIX = ['X', 'O', 'X', '', 'O', 'O', '', 'X', ''];
-  const MOVE_SIX = { squares: MOVE_VALUES_SIX };
+  const MoveValuesSix = ['X', 'O', 'X', '', 'O', 'O', '', 'X', ''];
+  const MoveSix = { squares: MoveValuesSix };
 
-  const MOVE_VALUES_SEVEN = ['X', 'O', 'X', 'X', 'O', 'O', '', 'X', ''];
-  const MOVE_SEVEN = { squares: MOVE_VALUES_SEVEN };
+  const MoveValuesSeven = ['X', 'O', 'X', 'X', 'O', 'O', '', 'X', ''];
+  const MoveSeven = { squares: MoveValuesSeven };
 
-  const MOVE_VALUES_EIGHT = ['X', 'O', 'X', 'X', 'O', 'O', '', 'X', 'O'];
-  const MOVE_EIGHT = { squares: MOVE_VALUES_EIGHT };
+  const MoveValuesEight = ['X', 'O', 'X', 'X', 'O', 'O', '', 'X', 'O'];
+  const MoveEight = { squares: MoveValuesEight };
 
-  const MOVE_VALUES_NINE = ['X', 'O', 'X', 'X', 'O', 'O', 'X', 'X', 'O'];
-  const MOVE_NINE = { squares: MOVE_VALUES_NINE };
+  const MoveValuesNine = ['X', 'O', 'X', 'X', 'O', 'O', 'X', 'X', 'O'];
+  const MoveNine = { squares: MoveValuesNine };
 
-  const HISTORY_OF_MOVES = [
-    MOVE_ONE,
-    MOVE_TWO,
-    MOVE_THREE,
-    MOVE_FOUR,
-    MOVE_FIVE,
-    MOVE_SIX,
-    MOVE_SEVEN,
-    MOVE_EIGHT,
-    MOVE_NINE,
+  const HistoryOfMoves = [
+    MoveONE,
+    MoveTwo,
+    MoveThree,
+    MoveFour,
+    MoveFive,
+    MoveSix,
+    MoveSeven,
+    MoveEight,
+    MoveNine,
   ];
 
   beforeAll(() => {
     wrapper = mount(
-      <Game history={HISTORY_OF_MOVES} moveNumber={8} onClick={mockOnClick} />
+      <Game history={HistoryOfMoves} moveNumber={8} onClick={mockOnClick} />
     );
   });
   it('checks if board exists', () => {
     expect(wrapper.find(Board).exists()).toBe(true);
   });
   it('checks if board has squares props', () => {
-    expect(wrapper.find(Board).props().squares).toEqual(MOVE_VALUES_NINE);
+    expect(wrapper.find(Board).props().squares).toEqual(MoveValuesNine);
   });
   it('checks onClick event on button', () => {
     wrapper.find('button.square').at(0).simulate('click');
@@ -96,14 +81,5 @@ describe('<Game/>', () => {
       .first()
       .text();
     expect(buttonText).toBe('O');
-    console.log('Button1: ', wrapper.find('button.square').at(0).html());
-    console.log('Button2: ', wrapper.find('button.square').at(1).html());
-    console.log('Button3: ', wrapper.find('button.square').at(2).html());
-    console.log('Button4: ', wrapper.find('button.square').at(3).html());
-    console.log('Button5: ', wrapper.find('button.square').at(4).html());
-    console.log('Button6: ', wrapper.find('button.square').at(5).html());
-    console.log('Button7: ', wrapper.find('button.square').at(6).html());
-    console.log('Button8: ', wrapper.find('button.square').at(7).html());
-    console.log('Button9: ', wrapper.find('button.square').at(8).html());
   });
 });
