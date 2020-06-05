f8c83d41c2cf19b17deb307858fb88ef945bb090
Alex <ialexjoshua@gmail.com>
2020-06-05 10:28:41 +0200

    fix: Added game status & updated CSS

--- src/components/game/component.jsx
+++ src/components/game/component.jsx
@@ -4,39 +4,45 @@ import PropTypes from 'prop-types';
 
 import Board from '../board';
 
-import { determineGameStatus } from './controller';
+import { determineGameStatus, calculateWinner } from './controller';
 
-const Game = (
-  { onSelectSquare, squares, previousPlayerMoves },
-  history,
+const Game = ({
+  onSelectSquare,
+  squares,
+  previousPlayerMoves,
   moveNumber,
-  xIsNext
-) => (
-  <Container className="game">
-    <div className="playerStatus">
-      {determineGameStatus(moveNumber, xIsNext)}
-    </div>
-    <div className="playerMoves">
-      {previousPlayerMoves.map(({ buttonName, buttonClick }, index) => (
-        <Button
-          className="moveHistory"
-          size="sm"
-          variant="outline-dark"
-          key={`${[index]}-${buttonName}`}
-          onClick={buttonClick}
-          data-qa="reset-to-move-history"
-        >
-          {buttonName}
-        </Button>
-      ))}
-    </div>
-    <Board
-      squares={squares}
-      onClick={(squareIndex) => onSelectSquare(squareIndex)}
-      data-qa="game-board"
-    />
-  </Container>
-);
+  xIsNext,
+}) => {
+  const winner = calculateWinner(squares);
+  return (
+    <Container className="game">
+      <div className="playerMoves">
+        <div className="playerStatus">
+          {determineGameStatus(winner, moveNumber, xIsNext)}
+        </div>
+        {previousPlayerMoves.map(({ buttonName, buttonClick }, index) => (
+          <Button
+            className="moveHistory"
+            size="sm"
+            variant="outline-dark"
+            key={`${[index]}-${buttonName}`}
+            onClick={buttonClick}
+            data-qa="reset-to-move-history"
+          >
+            {buttonName}
+          </Button>
+        ))}
+      </div>
+      <div className="gameBoard">
+        <Board
+          squares={squares}
+          onClick={(squareIndex) => onSelectSquare(squareIndex)}
+          data-qa="game-board"
+        />
+      </div>
+    </Container>
+  );
+};
 
 Game.propTypes = {
   onSelectSquare: PropTypes.func,
