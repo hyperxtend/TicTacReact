ddac212b67786cf9be939b3742e1215a3558ce4e
Alex <ialexjoshua@gmail.com>
2020-06-09 09:14:31 +0200

    fix: Simplified tests for controller

--- src/components/game/controller.test.jsx
+++ src/components/game/controller.test.jsx
@@ -1,47 +1,21 @@
 import { determineGameStatus } from './controller';
 
-describe('testing game status function', () => {
-  let playerX;
-  let playerO;
-  let nextPlayerOStatus;
-  let nextPlayerXStatus;
-  let drawGame;
-  let drawStatus;
-  let winnerX;
-  let winnerO;
-  let winnerXStatus;
-  let winnerOStatus;
+it('checks if player 2 is next', () => {
+  expect(determineGameStatus(null, 1, false)).toBe('Next player is O');
+});
 
-  beforeAll(() => {
-    playerX = 'Next player is X';
-    playerO = 'Next player is O';
-    winnerX = 'X is the Winner!';
-    winnerO = 'O is the Winner!';
-    drawGame = 'Its a Draw!';
-    nextPlayerOStatus = determineGameStatus(null, 1, false);
-    nextPlayerXStatus = determineGameStatus(null, 2, true);
-    winnerXStatus = determineGameStatus('X', 5, false);
-    winnerOStatus = determineGameStatus('O', 6, true);
-    drawStatus = determineGameStatus(false, 9, undefined);
-  });
+it('checks if player 1 is next', () => {
+  expect(determineGameStatus(null, 2, true)).toBe('Next player is X');
+});
 
-  it('checks if player O is next', () => {
-    expect(nextPlayerOStatus).toBe(playerO);
-  });
+it('checks if player 1 is the winner', () => {
+  expect(determineGameStatus('X', 5, false)).toBe('X is the Winner!');
+});
 
-  it('checks if player X is next', () => {
-    expect(nextPlayerXStatus).toBe(playerX);
-  });
+it('checks if player 2 is the winner', () => {
+  expect(determineGameStatus('O', 6, true)).toBe('O is the Winner!');
+});
 
-  it('checks if player X is the winner', () => {
-    expect(winnerXStatus).toBe(winnerX);
-  });
-
-  it('checks if player X is the winner', () => {
-    expect(winnerOStatus).toBe(winnerO);
-  });
-
-  it('checks if game status for a draw game', () => {
-    expect(drawStatus).toBe(drawGame);
-  });
+it('checks game status for a draw game', () => {
+  expect(determineGameStatus(false, 9, undefined)).toBe('Its a Draw!');
 });
