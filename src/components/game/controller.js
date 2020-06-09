144b2401abc0ca41e32884414f4d54bd84ae6b46
Alex <ialexjoshua@gmail.com>
2020-06-09 10:55:56 +0200

    WIP: Added tests and comments for calculateWinner

--- src/components/game/controller.js
+++ src/components/game/controller.js
@@ -1,5 +1,8 @@
 export const calculateWinner = (squares) => {
+  // squares - an array of played moves
+  // lines - an array of arrays with the each possible winning combination
   const lines = [
+    // a, b, c - positions in the array for a winning combinations in the array
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -9,9 +12,15 @@ export const calculateWinner = (squares) => {
     [0, 4, 8],
     [2, 4, 6],
   ];
+  /* For loop that goes through & checks that the array has the
+   winner combination positions in it */
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
+
+    /* If statement checks the 1st winning position in the array
+     equals the other 2 positions the returns the 1st position */
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      // squares[a] - the 1st position in the array of the winning combination
       return squares[a];
     }
   }
--- src/components/game/controller.test.js
+++ src/components/game/controller.test.js
@@ -1,4 +1,25 @@
-import { determineGameStatus } from './controller';
+import { determineGameStatus, calculateWinner } from './controller';
+
+describe('calculateWinner', () => {
+  let squares;
+  it('checks array for winner from 1st position in the array', () => {
+    squares = ['X', '', '', 'X', 'O', '', 'X', 'O', ''];
+    expect(calculateWinner(squares)).toBe(squares[0]);
+  });
+
+  it('checks array for winner from 2nd position in the array', () => {
+    squares = ['', 'X', 'O', 'O', 'X', 'X', '', 'X', 'O'];
+    expect(calculateWinner(squares)).toBe(squares[1]);
+  });
+
+  it('checks array for winner from 3rd position in the array', () => {
+    squares = ['', '', 'X', 'O', 'X', '', 'X', 'O', ''];
+    expect(calculateWinner(squares)).toBe(squares[2]);
+  });
+  /*  Can't test for the winner of the 4th position and
+   onwards as it would just return the 1st position of that winning combination
+   */
+});
 
 describe('determineGameStatus', () => {
   it('checks if player 2 is next', () => {
