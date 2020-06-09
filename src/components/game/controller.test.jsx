c93f1616b0c3a7bc4f957c214a4394451d369f89
Alex <ialexjoshua@gmail.com>
2020-06-09 09:23:57 +0200

    fix: Added describe for tests

--- src/components/game/controller.test.jsx
+++ src/components/game/controller.test.jsx
@@ -1,21 +1,23 @@
 import { determineGameStatus } from './controller';
 
-it('checks if player 2 is next', () => {
-  expect(determineGameStatus(null, 1, false)).toBe('Next player is O');
-});
+describe('determineGameStatus', () => {
+  it('checks if player 2 is next', () => {
+    expect(determineGameStatus(null, 1, false)).toBe('Next player is O');
+  });
 
-it('checks if player 1 is next', () => {
-  expect(determineGameStatus(null, 2, true)).toBe('Next player is X');
-});
+  it('checks if player 1 is next', () => {
+    expect(determineGameStatus(null, 2, true)).toBe('Next player is X');
+  });
 
-it('checks if player 1 is the winner', () => {
-  expect(determineGameStatus('X', 5, false)).toBe('X is the Winner!');
-});
+  it('checks if player 1 is the winner', () => {
+    expect(determineGameStatus('X', 5, false)).toBe('X is the Winner!');
+  });
 
-it('checks if player 2 is the winner', () => {
-  expect(determineGameStatus('O', 6, true)).toBe('O is the Winner!');
-});
+  it('checks if player 2 is the winner', () => {
+    expect(determineGameStatus('O', 6, true)).toBe('O is the Winner!');
+  });
 
-it('checks game status for a draw game', () => {
-  expect(determineGameStatus(false, 9, undefined)).toBe('Its a Draw!');
+  it('checks game status for a draw game', () => {
+    expect(determineGameStatus(false, 9, undefined)).toBe('Its a Draw!');
+  });
 });
