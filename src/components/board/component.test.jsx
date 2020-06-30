5d8aa803e846e38f71a14d8e8d24bb8ff50717d9
Alex <ialexjoshua@gmail.com>
2020-06-30 09:00:43 +0200

    fix: Added tests for onClick on each button

--- src/components/board/component.test.jsx
+++ src/components/board/component.test.jsx
@@ -4,11 +4,52 @@ import { mount } from 'enzyme';
 import Board from './component';
 
 describe('<Board/>', () => {
-  it('onClick event works on buttons', () => {
-    const onClick = jest.fn();
-    const squares = Array(9).fill('');
-    const wrapper = mount(<Board squares={squares} onClick={onClick} />);
-    wrapper.find('button.square').first().simulate('click');
+  const onClick = jest.fn();
+  const squares = Array(9).fill('');
+  const wrapper = mount(<Board squares={squares} onClick={onClick} />);
+
+  it('onClick event works on 1st button', () => {
+    wrapper.find('button.square').at(0).simulate('click');
+    expect(onClick).toBeCalledWith(0);
+  });
+
+  it('onClick event works on 2nd button', () => {
+    wrapper.find('button.square').at(1).simulate('click');
+    expect(onClick).toBeCalledWith(0);
+  });
+
+  it('onClick event works on 3rd button', () => {
+    wrapper.find('button.square').at(2).simulate('click');
+    expect(onClick).toBeCalledWith(0);
+  });
+
+  it('onClick event works on 4th button', () => {
+    wrapper.find('button.square').at(3).simulate('click');
+    expect(onClick).toBeCalledWith(0);
+  });
+
+  it('onClick event works on 5th button', () => {
+    wrapper.find('button.square').at(4).simulate('click');
+    expect(onClick).toBeCalledWith(0);
+  });
+
+  it('onClick event works on 6th button', () => {
+    wrapper.find('button.square').at(5).simulate('click');
+    expect(onClick).toBeCalledWith(0);
+  });
+
+  it('onClick event works on 7th button', () => {
+    wrapper.find('button.square').at(6).simulate('click');
+    expect(onClick).toBeCalledWith(0);
+  });
+
+  it('onClick event works on 8th button', () => {
+    wrapper.find('button.square').at(7).simulate('click');
+    expect(onClick).toBeCalledWith(0);
+  });
+
+  it('onClick event works on 9th button', () => {
+    wrapper.find('button.square').at(8).simulate('click');
     expect(onClick).toBeCalledWith(0);
   });
 });
