a07ee52b96cba2b93f65827fd19aa67e53949a4c
Alex <ialexjoshua@gmail.com>
2020-07-01 16:46:34 +0200

    fix: Updated tests for reducer

--- src/reducers/state-of-game/reducer.test.js
+++ src/reducers/state-of-game/reducer.test.js
@@ -7,54 +7,20 @@ describe('reducer', () => {
   });
 
   it('checks state of reducer for SELECT_SQUARE action', () => {
-    const actionValues = {
-      type: 'SELECT_SQUARE',
-      payload: {
-        squareId: 0,
-        currentMovesPlayed: ['', 'X', '', '', '', '', '', '', ''],
-      },
-    };
-
     const stateValues = {
-      history: [
-        ['', '', '', '', '', '', '', '', ''],
-        ['', 'X', '', '', '', '', '', '', ''],
-      ],
-      xIsNext: false,
-      moveNumber: 1,
+      history: [['', '', '', '', '', '', '', '', '']],
+      xIsNext: true,
+      moveNumber: 0,
     };
-    expect(reducer(undefined, actionValues)).toStrictEqual(stateValues);
-  });
-
-  it('checks if SELECT_SQUARE action is returned', () => {
-    const payload = 3;
-    const expectedAction = {
-      type: 'SELECT_SQUARE',
-      payload,
-    };
-    expect(selectSquare(payload)).toStrictEqual(expectedAction);
+    expect(reducer(undefined, selectSquare)).toStrictEqual(stateValues);
   });
 
   it('checks state of reducer for GO_TO_MOVE action', () => {
-    const actionValues = {
-      type: 'GO_TO_MOVE',
-      payload: 0,
-    };
-
     const stateValues = {
       history: [['', '', '', '', '', '', '', '', '']],
       moveNumber: 0,
       xIsNext: true,
     };
-    expect(reducer(undefined, actionValues)).toStrictEqual(stateValues);
-  });
-
-  it('checks if GO_TO_MOVE action is returned', () => {
-    const payload = 5;
-    const expectedAction = {
-      type: 'GO_TO_MOVE',
-      payload,
-    };
-    expect(goToMove(payload)).toStrictEqual(expectedAction);
+    expect(reducer(undefined, goToMove)).toStrictEqual(stateValues);
   });
 });
