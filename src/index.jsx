ec6b42b17407071b55d0c8c685ab99840e5ffff5
Alex <ialexjoshua@gmail.com>
2020-07-06 15:18:26 +0200

    fix: Added navbar that uses react-router-dom

--- src/index.jsx
+++ src/index.jsx
@@ -2,19 +2,29 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './index.css';
 import TicTacToeApp from './reducers';
 import Game from './components/game';
 import * as serviceWorker from './serviceWorker';
+import NavBar from './components/nav-bar';
 
 const store = createStore(TicTacToeApp);
 const root = document.getElementById('root');
 
 render(
   <Provider store={store}>
-    <Game />
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route exact path="/HomePage" />
+        <Route path="/VSComputer" />
+        <Route path="/VSPlayer" component={Game} />
+        <Route path="/ScoreBoard" />
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   root
 );
