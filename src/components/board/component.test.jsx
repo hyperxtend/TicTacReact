b0f8bbec4687bb78e38c0f0399f12fba2a9bee00
Alex <ialexjoshua@gmail.com>
2020-07-07 10:49:48 +0200

    fix: Tests check value onClick is called with

--- src/components/board/component.test.jsx
+++ src/components/board/component.test.jsx
@@ -15,41 +15,41 @@ describe('<Board/>', () => {
 
   it('onClick event works on 2nd button', () => {
     wrapper.find('button.square').at(1).simulate('click');
-    expect(onClick).toBeCalledWith(0);
+    expect(onClick).toBeCalledWith(1);
   });
 
   it('onClick event works on 3rd button', () => {
     wrapper.find('button.square').at(2).simulate('click');
-    expect(onClick).toBeCalledWith(0);
+    expect(onClick).toBeCalledWith(2);
   });
 
   it('onClick event works on 4th button', () => {
     wrapper.find('button.square').at(3).simulate('click');
-    expect(onClick).toBeCalledWith(0);
+    expect(onClick).toBeCalledWith(3);
   });
 
   it('onClick event works on 5th button', () => {
     wrapper.find('button.square').at(4).simulate('click');
-    expect(onClick).toBeCalledWith(0);
+    expect(onClick).toBeCalledWith(4);
   });
 
   it('onClick event works on 6th button', () => {
     wrapper.find('button.square').at(5).simulate('click');
-    expect(onClick).toBeCalledWith(0);
+    expect(onClick).toBeCalledWith(5);
   });
 
   it('onClick event works on 7th button', () => {
     wrapper.find('button.square').at(6).simulate('click');
-    expect(onClick).toBeCalledWith(0);
+    expect(onClick).toBeCalledWith(6);
   });
 
   it('onClick event works on 8th button', () => {
     wrapper.find('button.square').at(7).simulate('click');
-    expect(onClick).toBeCalledWith(0);
+    expect(onClick).toBeCalledWith(7);
   });
 
   it('onClick event works on 9th button', () => {
     wrapper.find('button.square').at(8).simulate('click');
-    expect(onClick).toBeCalledWith(0);
+    expect(onClick).toBeCalledWith(8);
   });
 });
