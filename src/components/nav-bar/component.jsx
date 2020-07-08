b7d1b287186822f78c5b254555d40da56adee97b
Alex <ialexjoshua@gmail.com>
2020-07-08 13:05:20 +0200

    fix: Created shared component for routes

--- src/components/nav-bar/component.jsx
+++ src/components/nav-bar/component.jsx
@@ -1,36 +1,49 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
 import { Navbar } from 'react-bootstrap';
 
+import Game from '../game';
+
 const NavbarLink = (props) => (
-  <NavLink className={props.className} to={props.routePath}>
+  <NavLink
+    className={props.className}
+    to={props.urlPath}
+    component={props.component}
+  >
     {props.linkTitle}
   </NavLink>
+);
+
+const NavbarRoutes = (props) => (
+  <Route path={props.urlPath} component={props.component} />
 );
 
 const NavigationBar = () => (
   <Navbar className="navigationBar">
     <NavbarLink
       className="homeLink"
-      routePath="/HomePage"
+      urlPath="/HomePage"
       linkTitle="Tic-Tac-Toe"
     />
     <NavbarLink
       className="navLink"
-      routePath="/VSComputer"
+      urlPath="/VSComputer"
       linkTitle="VS Computer"
     />
+    <NavbarLink className="navLink" urlPath="/VSPlayer" linkTitle="VS Player" />
     <NavbarLink
       className="navLink"
-      routePath="/VSPlayer"
-      linkTitle="VS Player"
-    />
-    <NavbarLink
-      className="navLink"
-      routePath="/ScoreBoard"
+      urlPath="/ScoreBoard"
       linkTitle="Score Board"
     />
   </Navbar>
 );
 
-export default NavigationBar;
+const PageNavigation = () => (
+  <div>
+    <NavigationBar />
+    <NavbarRoutes urlPath="/VSPlayer" component={Game} />
+  </div>
+);
+
+export default PageNavigation;
--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './index.css';
 import TicTacToeApp from './reducers';
-import Game from './components/game';
 import * as serviceWorker from './serviceWorker';
-import NavBar from './components/nav-bar';
+import PageNavigation from './components/nav-bar';
 
 const store = createStore(TicTacToeApp);
 const root = document.getElementById('root');
@@ -17,12 +16,8 @@ const root = document.getElementById('root');
 render(
   <Provider store={store}>
     <BrowserRouter>
-      <NavBar />
       <Switch>
-        <Route exact path="/HomePage" />
-        <Route path="/VSComputer" />
-        <Route path="/VSPlayer" component={Game} />
-        <Route path="/ScoreBoard" />
+        <PageNavigation />
       </Switch>
     </BrowserRouter>
   </Provider>,
