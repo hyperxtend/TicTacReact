aaa7d26b1ff3e1d404705c02069f46f1e1da430d
Alex <ialexjoshua@gmail.com>
2020-07-17 11:00:20 +0200

    fix: Changed landing page url

--- src/routes/urls.js
+++ src/routes/urls.js
@@ -1,4 +1,4 @@
-export const LANDING_PAGE = '/TicTacReact';
+export const LANDING_PAGE = '/';
 export const COMPUTER = '/Computer';
 export const PLAYER = '/Player';
 export const SCORE_BOARD = '/ScoreBoard';
