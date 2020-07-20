97788e557dd5ae2ab9ae7a63fdbb7a951cec4762
Alex <ialexjoshua@gmail.com>
2020-07-20 16:12:03 +0200

    feat: Setup scoreboard page

---
+++ src/pages/score-board/component.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Table } from 'react-bootstrap';
+
+import './component.css';
+
+const ScoreBoard = () => (
+  <Table className="score-table">
+    <thead>
+      <tr>
+        <th className="score-head">Player X</th>
+        <th className="score-head" colSpan={3}>
+          {' '}
+        </th>
+        <th className="score-head">Player O</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th className="score-sub-head">Wins:</th>
+        <td className="scores">0</td>
+        <th className="score-sub-head">Draws:</th>
+        <td className="scores">0</td>
+        <th className="score-sub-head">Wins:</th>
+        <td className="scores">0</td>
+      </tr>
+      <tr>
+        <td className="score-sub-head">Average Wins:</td>
+        <td className="scores">0%</td>
+        <td className="score-sub-head"> </td>
+        <td className="scores"> </td>
+        <td className="score-sub-head"> Average Wins:</td>
+        <td className="scores">0%</td>
+      </tr>
+    </tbody>
+  </Table>
+);
+
+export default ScoreBoard;
--- src/routes/component.jsx
+++ src/routes/component.jsx
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import LandingPage from '../pages/landing-page';
 import PlayAgainstFriend from '../pages/play-against-friend';
+import ScoreBoard from '../pages/score-board';
 
 import { LANDING_PAGE, COMPUTER, PLAYER, SCORE_BOARD } from './urls';
 
@@ -16,7 +17,7 @@ const NavigationRoutes = () => (
     />
     <Route path={COMPUTER} data-qa="vs-computer" />
     <Route path={PLAYER} component={PlayAgainstFriend} data-qa="vs-player" />
-    <Route path={SCORE_BOARD} data-qa="score-board" />
+    <Route path={SCORE_BOARD} component={ScoreBoard} data-qa="score-board" />
   </Switch>
 );
 
