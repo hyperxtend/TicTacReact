797ff31de033628df9906e2400e186eca5987c59
Alex <ialexjoshua@gmail.com>
2020-07-20 11:31:19 +0200

    fix: Updated Reducer tests

--- src/reducers/state-of-game/reducer.test.js
+++ src/reducers/state-of-game/reducer.test.js
@@ -1,5 +1,5 @@
 import reducer, { initialState } from './reducer';
-import { selectSquare, goToMove } from './actions';
+import { selectSquare, goToMove, computerMove } from './actions';
 
 describe('reducer', () => {
   it('checks initial state', () => {
@@ -8,19 +8,69 @@ describe('reducer', () => {
 
   it('checks state of reducer for SELECT_SQUARE action', () => {
     const stateValues = {
-      history: [['', '', '', '', '', '', '', '', '']],
-      xIsNext: true,
-      moveNumber: 0,
+      history: [
+        Array(9).fill(''),
+        ['X', '', '', '', '', '', '', '', ''],
+        ['X', '', '', '', '', 'O', '', '', ''],
+      ],
+      xIsNext: false,
+      moveNumber: 2,
     };
-    expect(reducer(undefined, selectSquare)).toStrictEqual(stateValues);
+    const expected = {
+      history: [
+        Array(9).fill(''),
+        ['X', '', '', '', '', '', '', '', ''],
+        ['X', '', '', '', '', 'O', '', '', ''],
+        undefined,
+      ],
+      xIsNext: true,
+      moveNumber: 3,
+    };
+    expect(reducer(stateValues, selectSquare(3))).toEqual(expected);
   });
 
   it('checks state of reducer for GO_TO_MOVE action', () => {
     const stateValues = {
-      history: [['', '', '', '', '', '', '', '', '']],
-      moveNumber: 0,
+      history: [
+        Array(9).fill(''),
+        ['X', '', '', '', '', '', '', '', ''],
+        ['X', '', '', '', '', 'O', '', '', ''],
+        ['X', '', '', 'X', '', 'O', '', '', ''],
+      ],
+      moveNumber: 3,
+      xIsNext: false,
+    };
+    const expected = {
+      history: [Array(9).fill(''), ['X', '', '', '', '', '', '', '', '']],
+      moveNumber: 1,
+      xIsNext: false,
+    };
+    expect(reducer(stateValues, goToMove(1))).toStrictEqual(expected);
+  });
+  it('checks state of reducer for COMPUTER_MOVE action', () => {
+    const stateValues = {
+      history: [
+        Array(9).fill(''),
+        ['X', '', '', '', '', '', '', '', ''],
+        ['X', '', '', '', '', 'O', '', '', ''],
+        ['X', '', '', 'X', '', 'O', '', '', ''],
+        ['X', 'O', '', 'X', '', 'O', '', '', ''],
+      ],
+      moveNumber: 4,
       xIsNext: true,
     };
-    expect(reducer(undefined, goToMove)).toStrictEqual(stateValues);
+    const expected = {
+      history: [
+        Array(9).fill(''),
+        ['X', '', '', '', '', '', '', '', ''],
+        ['X', '', '', '', '', 'O', '', '', ''],
+        ['X', '', '', 'X', '', 'O', '', '', ''],
+        ['X', 'O', '', 'X', '', 'O', '', '', ''],
+        undefined,
+      ],
+      moveNumber: 5,
+      xIsNext: true,
+    };
+    expect(reducer(stateValues, computerMove(5))).toStrictEqual(expected);
   });
 });
