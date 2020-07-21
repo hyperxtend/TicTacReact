b19aba3937744f7278899562cedca9a8f73f12a7
Alex <ialexjoshua@gmail.com>
2020-07-21 13:37:24 +0200

    fix: Updated tests for each url & its component

--- src/routes/component.test.jsx
+++ src/routes/component.test.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { shallow } from 'enzyme';
 
 import LandingPage from '../pages/landing-page';
+import PlayAgainstComputer from '../pages/play-against-computer';
+import PlayAgainstFriend from '../pages/play-against-friend';
 
 import NavigationRoutes from './component';
-import { LANDING_PAGE } from './urls';
+import { LANDING_PAGE, COMPUTER, FRIEND } from './urls';
+
+const urlPaths = {};
 
 describe('<NavigationRoutes/>', () => {
-  it('checks if route navigates to landing page', () => {
-    const wrapper = shallow(
-      <MemoryRouter initialEntries={[LANDING_PAGE]}>
-        <NavigationRoutes />
-      </MemoryRouter>
-    );
-    expect(wrapper.find(LandingPage)).toHaveLength(0);
+  beforeAll(() => {
+    const wrapper = shallow(<NavigationRoutes />);
+    wrapper.find(Route).reduce((_, route) => {
+      const routeProps = route.props();
+      urlPaths[routeProps.path] = routeProps.component;
+      return urlPaths;
+    }, {});
+  });
+
+  it('checks / url for Landing Page component', () => {
+    expect(urlPaths[LANDING_PAGE]).toBe(LandingPage);
+  });
+
+  it('checks /Computer url for verse computer component', () => {
+    expect(urlPaths[COMPUTER]).toBe(PlayAgainstComputer);
+  });
+
+  it('checks /Friend url for verse friend component', () => {
+    expect(urlPaths[FRIEND]).toBe(PlayAgainstFriend);
   });
 });
