d80661b3e09621efdc5d48db3fa03cb03e11c9c3
Alex <ialexjoshua@gmail.com>
2020-07-31 11:51:37 +0200

    fix: Added tests for local storage

---
+++ src/reducers/local-storage.test.js
@@ -0,0 +1,48 @@
+import { saveState, getState } from './local-storage';
+
+describe('localStorage', () => {
+  beforeEach(() => {
+    Object.defineProperty(window, 'localStorage', {
+      value: {
+        getItem: jest.fn(),
+        setItem: jest.fn(),
+      },
+      writable: true,
+    });
+  });
+
+  describe('getState', () => {
+    it('checks if getItem localStorage gets called', () => {
+      getState();
+      expect(window.localStorage.getItem).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('saveState', () => {
+    it('checks if setItem localStorage gets called', () => {
+      const mockCurrentState = {
+        app: {
+          status: {
+            history: [
+              ['', '', '', '', '', '', '', '', ''],
+              ['', 'X', '', '', '', '', '', '', 'O'],
+              ['X', 'X', 'O', '', '', '', '', '', 'O'],
+              ['X', 'X', 'O', '', '', 'X', '', 'O', 'O'],
+              ['X', 'X', 'O', '', '', 'X', '', 'O', 'O'],
+              ['X', 'X', 'O', 'X', 'O', 'X', '', 'O', 'O'],
+            ],
+            xIsNext: true,
+            winner: '',
+            moveNumber: 5,
+            playerXScore: 1,
+            playerOScore: 0,
+            drawScore: 0,
+            gamesPlayed: 1,
+          },
+        },
+      };
+      saveState(mockCurrentState);
+      expect(window.localStorage.setItem).toHaveBeenCalledTimes(1);
+    });
+  });
+});
