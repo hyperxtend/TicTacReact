f8b4dc93d658df8d3a5ff51615a8953dd5f1edda
Alex <ialexjoshua@gmail.com>
2020-09-03 17:36:49 +0200

    fix: Added styled components for NavBar

--- src/components/nav-bar/component.jsx
+++ src/components/nav-bar/component.jsx
@@ -1,26 +1,49 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { Navbar } from 'react-bootstrap';
-
-import './component.css';
+import styled from 'styled-components';
 
 import { LANDING_PAGE, COMPUTER, FRIEND, SCORE_BOARD } from '../../routes/urls';
 
+const StyledNavBar = styled.nav`
+  width: 100vw;
+  display: flex;
+  background: #22272c;
+  min-height: 10vh;
+  align-items: center;
+  position: relative;
+`;
+
+const StyledNavLink = styled.a`
+  color: #e8e8e8;
+  cursor: pointer;
+  font-family: 'Roboto', sans-serif;
+  font-size: x-large;
+  font-weight: 800;
+  position: relative;
+  margin-right: 5%;
+  left: 30%;
+
+  &:hover {
+    color: #8c9ea3;
+    border-bottom: 5px solid #bbbbbe;
+    text-decoration: none;
+  }
+`;
+
 const NavigationBar = () => (
-  <Navbar className="navigation-bar">
-    <NavLink className="home-link" to={LANDING_PAGE}>
+  <StyledNavBar className="navigation-bar">
+    <StyledNavLink className="home-link" href={LANDING_PAGE}>
       Tic-Tac-Toe
-    </NavLink>
-    <NavLink className="nav-link" to={COMPUTER}>
+    </StyledNavLink>
+    <StyledNavLink className="nav-link" href={COMPUTER}>
       VS Computer
-    </NavLink>
-    <NavLink className="nav-link" to={FRIEND}>
+    </StyledNavLink>
+    <StyledNavLink className="nav-link" href={FRIEND}>
       VS Friend
-    </NavLink>
-    <NavLink className="nav-link" to={SCORE_BOARD}>
+    </StyledNavLink>
+    <StyledNavLink className="nav-link" href={SCORE_BOARD}>
       Score Board
-    </NavLink>
-  </Navbar>
+    </StyledNavLink>
+  </StyledNavBar>
 );
 
 export default NavigationBar;
