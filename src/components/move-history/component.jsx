8241997681abd89d2e390a561fa0d7b7bde8149b
Alex <ialexjoshua@gmail.com>
2020-09-04 11:09:20 +0200

    fix: Created styled components for MoveHistory

--- src/components/move-history/component.jsx
+++ src/components/move-history/component.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-import './component.css';
+const MoveHistoryContainer = styled.div`
+  width: 20%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 0;
+  margin-left: 3%;
+  text-align: center;
+  box-shadow: 0 0px 2px #00000029, 0 1px 3px #00000029;
+  padding: 2%;
+`;
+
+const HistoryHeader = styled.span`
+  font-size: 1.2rem;
+  text-transform: uppercase;
+  margin-bottom: 5%;
+  font-weight: 600;
+  color: #22272c;
+  letter-spacing: 2px;
+`;
 
 const MoveHistory = ({ previousPlayerMoves }) => (
-  <div className="player-moves">
-    <span className="history-header">Go back to</span>
+  <MoveHistoryContainer>
+    <HistoryHeader>Go back to</HistoryHeader>
     {previousPlayerMoves.map(({ buttonName, buttonClick }, index) => (
       <Button
         className="move-history"
@@ -19,7 +38,7 @@ const MoveHistory = ({ previousPlayerMoves }) => (
         {buttonName}
       </Button>
     ))}
-  </div>
+  </MoveHistoryContainer>
 );
 
 MoveHistory.propTypes = {
