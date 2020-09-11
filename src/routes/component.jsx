8e16c627cf4b4d15435bddcba75a7f65829b8000
Alex <ialexjoshua@gmail.com>
2020-09-11 18:09:44 +0200

    fix: Added route for base URL

--- src/routes/component.jsx
+++ src/routes/component.jsx
@@ -5,17 +5,13 @@ import LandingPage from '../pages/landing-page';
 import PlayAgainstComputer from '../pages/play-against-computer';
 import PlayAgainstFriend from '../pages/play-against-friend';
 import ScoreBoard from '../pages/score-board';
+import PageNotFound from '../pages/page-not-found';
 
 import { LANDING_PAGE, COMPUTER, FRIEND, SCORE_BOARD } from './urls';
 
 const NavigationRoutes = () => (
   <Switch>
-    <Route
-      exact
-      path={LANDING_PAGE}
-      component={LandingPage}
-      data-qa="landing-page"
-    />
+    <Route path={LANDING_PAGE} component={LandingPage} data-qa="landing-page" />
     <Route
       path={COMPUTER}
       component={PlayAgainstComputer}
@@ -23,6 +19,7 @@ const NavigationRoutes = () => (
     />
     <Route path={FRIEND} component={PlayAgainstFriend} data-qa="vs-player" />
     <Route path={SCORE_BOARD} component={ScoreBoard} data-qa="score-board" />
+    <Route component={PageNotFound} />
   </Switch>
 );
 
